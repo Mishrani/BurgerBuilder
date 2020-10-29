2313cc235a47eedf994d3a28066a7d092792bfa1
Mishrani <49362029+Mishrani@users.noreply.github.com>
2020-10-29 12:51:02 +0530

    dynamic ingredients added and proptype added

---
+++ src/containers/BurgerBuilder/BurgerBuiilder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class BurgerBuilder extends React.Component{
+    render(){
+        return(
+            <React.Fragment>
+               <div>Burger</div>
+               <div>Burger Builder</div>     
+            </React.Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder;
